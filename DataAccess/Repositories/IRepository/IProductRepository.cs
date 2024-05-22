56cb8c39de512cc82c1d720ce853df6f7f264163
banana-w <phuonghiepthuan56@gmail.com>
2024-05-22 13:15:56 +0700

    thay day tren lop

---
+++ DataAccess/Repositories/IRepository/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using BusinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkShopBusiness.Repositories.IRepository
+{
+    public interface IProductRepository
+    {
+        List<Product> GetAllProducts();
+        Product? GetProductByID(int id);
+        void AddNew(Product product);
+        void Update(Product product);
+        void Delete(int id);
+        void Save();
+    }
+}
